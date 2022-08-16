52ff5589e3678a241eb2d471edb6c563934a2591
pankaj4131 <pankajmanjare516@gmail.com>
2022-08-16 16:22:48 +0530

    first commit

---
+++ Day10_Array/src/com/cg/array/ArrayDemo.java
@@ -0,0 +1,18 @@
+package com.cg.array;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		
+		/*int arr[]= new int[3] {11,22,33};
+		do not declare the size at the end if you are initializing the array
+		*/
+		int arr[]=new int[] {11,22,33};
+		System.out.println("The array index value is : "+arr[1]);
+		
+	/*	Array index out of bound exception as index is 3 we are accessing and lastindex is 2
+		System.out.println("The array index value is : "+arr[3]);*/
+
+	}
+
+}
---
+++ Day10_Array/src/com/cg/array/StudentArray.java
@@ -0,0 +1,30 @@
+package com.cg.array;
+
+class Student
+{
+	public int roll_no;
+	public String name;
+	//Constructor
+	Student(int roll_no,String name)
+	{
+		this.roll_no=roll_no;
+		this.name=name;
+	}
+}
+
+public class StudentArray {
+
+	public static void main(String[] args) {
+		// Non-primitive Array memory accocation
+		Student[] arr=new Student[3];
+		arr[0]=new Student(101,"Pankaj");
+		arr[1]=new Student(102,"Rutuja");
+		arr[2]=new Student(103,"Sanjana");
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println("Element at "+i+"index: "+arr[i].roll_no+" "+arr[i].name);
+		}
+
+	}
+
+}
