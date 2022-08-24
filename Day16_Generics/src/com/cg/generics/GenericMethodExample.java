0e3f1f1695b0ed9c8e6a2b3eff5b16289a23dc52
pankaj4131 <pankajmanjare516@gmail.com>
2022-08-24 10:54:12 +0530

    first commit

---
+++ Day16_Generics/src/com/cg/generics/GenericMethodExample.java
@@ -0,0 +1,26 @@
+package com.cg.generics;
+
+public class GenericMethodExample {
+	//when we use generic method we can pass any types of arguments in the method
+		public static <E>void printArray(E[] elem)
+		{
+			for(E itr:elem)
+			{
+				System.out.println(itr.getClass().getName());//to check the className
+				System.out.println(itr);
+			}
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer[] arr1= {10,20,30};
+		Character [] arr2= {'z','x','a'};
+		System.out.println("Printing array for Integer");
+		printArray(arr1);
+		System.out.println("Printing array for Character");
+		printArray(arr2);
+
+
+	}
+
+}
---
+++ Day16_Generics/src/com/cg/generics/Sender.java
@@ -0,0 +1,22 @@
+package com.cg.generics;
+//generic class
+public class Sender <T>
+{
+	private T message;
+	//getter method
+	public T getMessage() {
+		return message;
+	}
+	//setter method
+	public void setMessage(T message) {
+		this.message = message;
+	}
+	public void sendMessage()
+	{
+		System.out.println("Message Type: "+getMessage().getClass());
+		System.out.println("Contents are: "+getMessage());
+
+	}
+	
+
+}
